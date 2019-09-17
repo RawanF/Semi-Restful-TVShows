fc156871bf8199bc9127aeaff001762b19e3b280
TV Shows <rawan.f.h@gmail.com>
2019-09-18 02:51:07 +0300

    Add basic validation to the forms

--- apps/showapp/views.py
+++ apps/showapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Show
+from django.contrib import messages
+
 
 def root(request):
     return redirect("/shows")
@@ -17,28 +19,47 @@ def DisplayShow(request, num):
 
 def newShow(request):
     if request.method == "POST":
-        t=request.POST["title"]
-        nw=request.POST["network"]
-        des=request.POST["description"]
-        RD=request.POST["RDate"]
-        sh=Show.objects.create(title=t,network=nw, desc= des,RDate=RD)
-        sh.save()
-        return redirect(f'/shows/{sh.id}' )
+        errors = Show.objects.basic_validator(request.POST)
+            # check if the errors dictionary has anything in it
+        if len(errors) > 0:
+            # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+            for key, value in errors.items():
+                messages.error(request, value)
+            # redirect the user back to the form to fix the errors
+            return render(request, "showapp/index2.html")
+        else:
+            t=request.POST["title"]
+            nw=request.POST["network"]
+            des=request.POST["desc"]
+            RD=request.POST["RDate"]
+            sh=Show.objects.create(title=t,network=nw, desc= des,RDate=RD)
+            sh.save()
+            return redirect(f'/shows/{sh.id}' )
     return render(request, "showapp/index2.html")
 
 def editShow(request, num):
     if request.method == "POST":
-        t=request.POST["title"]
-        nw=request.POST["network"]
-        des=request.POST["description"]
-        RD=request.POST["RDate"]
-        sh=Show.objects.get(id=num)
-        sh.title=t
-        sh.network=nw
-        sh.desc=des
-        sh.RDate=RD
-        sh.save()
-        return redirect(f'/shows/{sh.id}' )
+        errors = Show.objects.basic_validator(request.POST)
+            # check if the errors dictionary has anything in it
+        if len(errors) > 0:
+            # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+            for key, value in errors.items():
+                messages.error(request, value)
+            # redirect the user back to the form to fix the errors
+            return redirect(f'/shows/{num}/edit' , messages)
+        else:
+            t=request.POST["title"]
+            nw=request.POST["network"]
+            des=request.POST["desc"]
+            # des=request.POST.get("desc","No description")
+            RD=request.POST["RDate"]
+            sh=Show.objects.get(id=num)
+            sh.title=t
+            sh.network=nw
+            sh.desc=des
+            sh.RDate=RD
+            sh.save()
+            return redirect(f'/shows/{sh.id}')
     context = {
     	"show": Show.objects.get(id=num),
     }
